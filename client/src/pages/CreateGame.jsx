f543e2c6f1d9f6e77d052fbbb77e537f8b66b4be
amaltasic-oss <amaltasic@gmail.com>
2026-03-30 23:23:05 +0200

    Update CreateGame.jsx

--- client/src/pages/CreateGame.jsx
+++ client/src/pages/CreateGame.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import API_URL from "../api";
 
 function CreateGame() {
   const [name, setName] = useState("");
